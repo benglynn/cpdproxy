b32fc59ea8390de0ebd6e5a9f14f606c9852848f
Ben Glynn <ben.glynn@just-eat.com>
2016-12-23 11:20:57 +0000

    Tidied whitespace

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const http = require('http'),
-      httpProxy = require('http-proxy');
-      port = 8080;
+  httpProxy = require('http-proxy');
+  port = 8080;
 
 var proxy = httpProxy.createProxyServer({});
 var server = http.createServer(function(req, res) {
-    proxy.web(req, res, {
-          target: 'https://www.just-eat.co.uk',
-          changeOrigin: true
-      });
+  proxy.web(req, res, {
+    target: 'https://www.just-eat.co.uk',
+    changeOrigin: true
+  });
 });
 
 console.log(`listening on port ${port}`);
