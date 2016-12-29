e678cf55ad842e362c969cc09ce04fe752b244b1
Ben Glynn <ben.glynn@just-eat.com>
2016-12-29 07:10:18 +0000

    Transform proxy with trumpet

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-const http = require('http'),
-  httpProxy = require('http-proxy');
-  port = 8080;
+const http = require('http');
+const connect = require('connect');
+const httpProxy = require('http-proxy');
+const harmon = require('harmon');
 
-var proxy = httpProxy.createProxyServer({});
-var server = http.createServer(function(req, res) {
-  proxy.web(req, res, {
-    target: 'https://www.just-eat.co.uk',
-    changeOrigin: true
-  });
+const port = 8080;
+const proxy = httpProxy.createProxyServer({
+  target: 'https://www.just-eat.co.uk',
+  changeOrigin: true
 });
 
+let selects = [{
+  query: 'h1',
+  func: function (node) {
+    node.createWriteStream().end('Stream transform!');
+  }
+}];
+
+let app = connect();
+app.use(harmon([], selects, true));
+app.use( (req, res) => {proxy.web(req, res);});
+
 console.log(`listening on port ${port}`);
-server.listen(port);
+http.createServer(app).listen(port);
